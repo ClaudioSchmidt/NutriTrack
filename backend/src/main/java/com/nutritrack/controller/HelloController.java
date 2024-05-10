2c3b7995a6123377c3833e4e1eb98d8df878a2eb
clscit <147387660+clscit@users.noreply.github.com>
2024-05-10 13:22:03 +0200

    Backend setup

---
+++ backend/src/main/java/com/nutritrack/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.nutritrack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "NutriTrack backend is running!";
+    }
+}
